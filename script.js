5d9d6706eb4983fcbfa71bf88b160f2df4871cf4
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-26 01:48:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let table = document.getElementById('sampleTable')
+
 function insert_Row() {
     //Write your code here
-  
-  
+	let newRow = table.insertRow(0);
+	let newCell1 = newRow.insertCell(0);
+	let newCell2 = newRow.insertCell(1);
+
+	newCell1.innerText = 'New Cell1';
+	newCell2.innerText = 'New Cell2';
 }
